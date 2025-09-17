bb61fb1e6c0fd08eab724140870a622850d385a5
soukaina <soukaina.asaas.slc25@gmail.com>
2025-09-17 13:06:38 +0100

    first commit

---
+++ js.js
@@ -0,0 +1,15 @@
+// variables
+let nom = "soukaina";
+const PI = 3.14;
+console.log("Bonjour " + nom +"!");
+
+
+// les fonctions 
+
+function addition(a, b) {
+    return a * b;
+  
+  }
+  
+  let multiplication = addition(4, 5);
+  console.log("La multiplication  est : " + multiplication);
