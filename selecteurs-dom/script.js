2f3b7782f739108e32301e0ed687b888ff47e57e
soukaina <soukaina.asaas.slc25@gmail.com>
2025-10-15 09:12:48 +0100

    add js courses

--- selecteurs-dom/script.js
+++ selecteurs-dom/script.js
@@ -1,5 +1,14 @@
+
+
+
 let titre = document.getElementById("titre-principal");
 console.log("Texte du titre : " + titre.innerText);
 
+const title = document.getElementById("btn");
+title.style.color="red";
+
 let bouton = document.querySelector(".btn");
 console.log("Texte du bouton : " + bouton.innerText);
+
+
+
